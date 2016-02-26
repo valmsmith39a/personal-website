a1bbd3290fdb8dc03df13390b90affad8d3a1af7
valmsmith39a <valmsmith39a@gmail.com>
2016-02-26 01:21:18 -0800

    Contact box

--- javascript/controllers/homeCtrl.js
+++ javascript/controllers/homeCtrl.js
@@ -5,4 +5,18 @@ app.controller('homeCtrl', function($scope, $location, $anchorScroll) {
     $location.hash(id);
     $anchorScroll();
   }
+
+  $scope.submitContactInfo = function() {
+    console.log('contact first name: ', $scope.contact);
+    Contact.submit($scope.contact)
+    .then(function(res) {
+      console.log('successfully sent res is: ', res);
+    });
+  };
+});
+
+app.service('Contact', function($http) {
+  this.submit = function(contact) {
+    return $http.post('https://mandrillapp.com/api/1.0/messages/send.json', contact);
+  };
 });
