becb6da6d2aaed441098a592bac4af7d080319ad
valmsmith39a <valmsmith39a@gmail.com>
2016-03-01 00:26:04 -0800

    Updated resume

--- javascript/module.js
+++ javascript/module.js
@@ -6,5 +6,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
   .state('home', {url:'/', templateUrl:'./partials/home.html', controller:'homeCtrl'})
   .state('blog', {url:'/blog', templateUrl:'./partials/blog.html', controller:'blogCtrl'})
+  .state('resumePDF', {url:'/resume_pdf', templateUrl:'./partials/resumePDF.html'})
+  .state('resumeDOC', {url:'/resume_doc', templateUrl:'./partials/resumeDOC.html'})
   $urlRouterProvider.otherwise('/');
 });
